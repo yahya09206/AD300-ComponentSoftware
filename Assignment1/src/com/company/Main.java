b8b29296b65463fe00b4d055f7f996fe1b426f62
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-09 22:38:04 -0700

    cumulative sum counter

--- Assignment1/src/com/company/Main.java
+++ Assignment1/src/com/company/Main.java
@@ -7,6 +7,9 @@ public class Main {
 	// write your code here
         //Declare scanner object
         Scanner console = new Scanner(System.in);
+        System.out.println("How many numbers would you like to see the average for?");
+        int numbers = console.nextInt();
+        int sum = 0;
 
     }
 }
