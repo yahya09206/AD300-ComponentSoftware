fd07d1fcbec00bbf1a986a015a2a3892439e50a1
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-16 23:17:57 -0700

    fixed error where file wasnt being read

--- Assignment2/src/com/company/Main.java
+++ Assignment2/src/com/company/Main.java
@@ -1,16 +1,20 @@
 
 package com.company;
 
+
+import java.io.FileNotFoundException;
 import java.util.*;
+import java.io.File;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    //Create an arraylist of all words inside of txt file
-        ArrayList<String> txtWords = new ArrayList<String>();
+    public static void main(String[] args) throws FileNotFoundException {
 
         //new scanner object
-        Scanner input = new Scanner(new File("words.txt"));
+        Scanner input = new Scanner(new File("wordscopy.txt"));
+
+        //Create an arraylist of all words inside of txt file
+        ArrayList<String> txtWords = new ArrayList<>();
 
         //read through file and add words to array
         while (input.hasNext()){
