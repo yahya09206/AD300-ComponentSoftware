ff53fcf8861ad75ed038faa8a1ecb8bc9a6dc798
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-16 22:30:39 -0700

    scanned txt file

--- Assignment2/src/com/company/Main.java
+++ Assignment2/src/com/company/Main.java
@@ -6,7 +6,19 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	//Create an arraylist of all words inside of txt file
+	    //Create an arraylist of all words inside of txt file
         ArrayList<String> txtWords = new ArrayList<String>();
+
+        //new scanner object
+        Scanner input = new Scanner(new File("words.txt"));
+
+        //read through file and add words to array
+        while (input.hasNext()){
+            String word = input.next();
+            txtWords.add(word);
+        }
+
+        //print out words
+        System.out.println(txtWords);
     }
 }
