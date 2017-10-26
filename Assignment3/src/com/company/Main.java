68de83a5d0482d6a887dddb4a2bc3d6d2407373d
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-25 19:18:38 -0700

    reading from file

--- Assignment3/src/com/company/Main.java
+++ Assignment3/src/com/company/Main.java
@@ -7,7 +7,10 @@ import java.io.File;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
+
+        //New Scanner object
+        Scanner input = new Scanner(new File("agile_manifesto.txt"));
     }
 }
