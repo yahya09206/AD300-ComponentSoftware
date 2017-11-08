b14ff9e6334ad96ede197861b9e123a0cfc02253
Yahya Hussein <yahyahussein91@gmail.com>
2017-11-07 19:55:40 -0800

    finished with bubble sort

--- Assignment5/src/com/company/Main.java
+++ Assignment5/src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //create an int array we want to sort using bubble sort algorithm
+        int intArray[] = new int[]{30,1,15,9,300,54};
+
+
+        //sort an array using bubble sort algorithm
+        bubbleSort(intArray);
+
+        System.out.println("");
+
+        //print array after sorting
+        System.out.println("Array After Bubble Sort");
+        for(int i=0; i < intArray.length; i++){
+            System.out.print(intArray[i] + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] arr){
+        int n = arr.length;
+        int temp = 0;
+
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+
+                if(arr[j-1] > arr[j]){
+                    //swap the elements!
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
     }
 }
