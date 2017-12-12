aeb01766184d9c2127f73ed0f9b11df2a18a97e5
Yahya Hussein <yahyahussein91@gmail.com>
2017-12-11 18:32:04 -0800

    finished with assign7

--- Assignment7/src/com/company/Main.java
+++ Assignment7/src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //item list
+        String[] items = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+        // instantiation of LinkedList
+        List<String> list = new LinkedList<String>();
+
+        // add new items to LinkedList
+        Collections.addAll(list, items);
+
+        // prints out the original list
+        System.out.println("Original List =>" + list);
+
+        // reverses the list
+        Collections.reverse(list);
+
+        // prints out the new reversed list
+        System.out.println("Reversed List =>" + list);
     }
 }
